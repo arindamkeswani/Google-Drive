fc455327fa9a25023df8617ee9f1318bb0407fc8
Arindam <arindamkeswani@gmail.com>
2022-01-03 18:26:18 +0530

    Database skeleton created

---
+++ Backend/controller/dataController.js
@@ -0,0 +1,96 @@
+const { query } = require('express');
+const mysql = require('mysql');
+
+//connection pool
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "gdrive",
+    port: 3306
+});
+
+
+//get root data
+exports.viewFolders = (req, res) => {
+    // res.render('home');
+
+
+    //Connect to DB
+    pool.getConnection((err, connection) => {
+        if (err)
+            throw err; //not connected
+        console.log('Connected as ID (data controller View DB ): ', connection.threadId);
+
+
+        // use the connection
+        parent_folder = "root"
+        result = []
+        connection.query('SELECT * FROM folders where user_id=1 AND parent_folder=?', [parent_folder], (err, rows) => {
+            //When done with the connection, release it
+            // connection.release();
+
+            if (!err) {
+
+                // result.push(rows)
+                res.send({
+                    query_returned: rows
+                })
+                // console.log("Rows in folder:",rows);
+            } else {
+                console.log(err);
+            }
+
+        });
+
+        // connection.query('SELECT * FROM notepads where user_id=1 AND parent_folder=?', [parent_folder], (err, rows) => {
+        //     connection.release();
+
+        //     if (!err) {
+
+        //         // result.push(rows)
+        //         res.write(
+        //             JSON.stringify({query_returned: rows})
+        //         )
+        //         // console.log("Rows in folder:",rows);
+        //     } else {
+        //         console.log(err);
+        //     }
+        // });
+
+    })
+};
+
+exports.viewNotepads = (req, res) => {
+    // res.render('home');
+
+
+    //Connect to DB
+    pool.getConnection((err, connection) => {
+        if (err)
+            throw err; //not connected
+        console.log('Connected as ID (data controller View DB ): ', connection.threadId);
+
+
+        // use the connection
+        parent_folder = "root"
+
+
+        connection.query('SELECT * FROM notepads where user_id=1 AND parent_folder=?', [parent_folder], (err, rows) => {
+            connection.release();
+
+            if (!err) {
+
+                // result.push(rows)
+                res.send(
+                    { query_returned: rows }
+                )
+                console.log("Rows in folder:", rows);
+            } else {
+                console.log(err);
+            }
+        });
+
+    })
+};
