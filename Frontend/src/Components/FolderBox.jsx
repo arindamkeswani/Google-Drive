979abe27199fa080db6df703a9d986cff6442539
arpit-maurya <arpitmaurya161@gmail.com>
2022-01-05 17:06:14 +0530

    folderBox-click_fix

--- Frontend/src/Components/FolderBox.jsx
+++ Frontend/src/Components/FolderBox.jsx
@@ -17,9 +17,6 @@ function Drive_FolderBox(props) {
        class='folderBox'
        id={folderData.id}
        parent_folder={folderData.parent_folder}
-       onClick={() => {
-         display_folder_id(folderData.id, folderData.folder_name);
-       }}
      >
        <div class='editBox'>
          <div
@@ -45,7 +42,12 @@ function Drive_FolderBox(props) {
            </span>
          </div>
        </div>
-       <div class='folderIcon'>
+       <div
+         class='folderIcon'
+         onClick={() => {
+           display_folder_id(folderData.id, folderData.folder_name);
+         }}
+       >
          <span class='material-icons'> folder </span>
        </div>
        <div class='folderName'>{folderData.folder_name}</div>
